866878f4591e188a0922cfda10c8977e24b23223
yyyoon <60565912+udraon@users.noreply.github.com>
2024-04-18 01:00:35 +0900

    InitProject2
    24.04.18
    - 패키지명 변경
    - webapp/resources/css, js 추가
    - bootstrap 설정 추가
    - sample dashboard 테마 추가

---
+++ UnionSpace/src/main/java/com/gd/uspace/main/MainService.java
@@ -0,0 +1,9 @@
+package com.gd.uspace.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainService {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
