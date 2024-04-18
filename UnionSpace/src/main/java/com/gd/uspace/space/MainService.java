da885d8e85cdc806e79f4967b1c63b8550a0dbfe
yoonyuyong <udraon@naver.com>
2024-04-18 10:13:02 +0900

    Revert "24.04.18"
    This reverts commit 4d4240cc657b41618d4fd299b23a9dc7803108cd.

--- UnionSpace/src/main/java/com/gd/uspace/space/MainService.java
+++
@@ -1,9 +0,0 @@
-package com.gd.uspace.space;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class MainService {
-	Logger logger = LoggerFactory.getLogger(this.getClass());
-
-}
