f9588f37137976435912e3c2da2efb7034c5249d
kim_jeong_won <comkjo0512@gmail.com>
2024-04-18 11:56:01 +0900

    최초 프로젝트

---
+++ src/main/java/com/gd/uspace/space/MainService.java
@@ -0,0 +1,9 @@
+package com.gd.uspace.space;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainService {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
