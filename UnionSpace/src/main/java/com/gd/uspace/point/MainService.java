0173a2971b5500e6e3b8dccb226427c034b8df40
yoonyuyong <udraon@naver.com>
2024-04-18 10:14:00 +0900

    Reapply "24.04.18" - java폴더에 package 추가 - views폴더에 뷰 폴더 추가 각 해당 파일들 이름 변경해서 사용!!! (최대한 서로 다른 폴더나 패키지 사용하는걸 권장)
    This reverts commit da885d8e85cdc806e79f4967b1c63b8550a0dbfe.

---
+++ UnionSpace/src/main/java/com/gd/uspace/point/MainService.java
@@ -0,0 +1,9 @@
+package com.gd.uspace.point;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainService {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
